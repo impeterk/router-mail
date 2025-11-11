237922a5850da8b8e97f773a2fda1d54d8de5905
peterk <peterkudelas@gmail.com>
2025-11-11 16:18:05 +0100

    add devtools

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,13 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import { reactRouterDevTools } from "react-router-devtools";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const RRDevTools = process.env.RDT ? reactRouterDevTools() : null;
 
 export default defineConfig({
   plugins: [
+    RRDevTools,
     reactRouter(),
     tsconfigPaths({
       projects: ["./tsconfig.json"],
